cb9a3d9d7267dc581b0406fb1eacec3fb1aaf212
romann-broque <kinoide56@gmail.com>
2022-12-09 14:18:18 +0100

    add relief

--- srcs/struct/map.c
+++ srcs/struct/map.c
@@ -6,17 +6,45 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 16:04:04 by rbroque           #+#    #+#             */
-/*   Updated: 2022/12/07 23:47:35 by rbroque          ###   ########.fr       */
+/*   Updated: 2022/12/09 14:17:12 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+int		get_x_offset(int x, int zoom, int i)
+{
+	(void)i;
+	return (x + zoom);
+}
+
+int		get_y_offset(int y, int z, int last_z, int zoom, int i)
+{
+	(void)i;
+	(void)zoom;
+	if (last_z < z)
+		return (y - 1);
+	if (last_z > z)
+		return (y + 1);
+	return (y);
+}
+
+t_pos	*get_curr_pos(int *x, int *y, int *z, int last_z, int zoom, int i)
+{
+	*x = get_x_offset(*x, zoom, i);
+	*y = get_y_offset(*y, *z, last_z, zoom, i);
+	return (init_pos(*x + HEIGHT / 5,
+					*y * (zoom) + WIDTH / 5,
+					*z));
+}
 
 t_pos	**get_pos_array(char **coord, int y, int zoom)
 {
 	const size_t	size = get_array_size_char(coord);
 	size_t			i;
 	int				x;
+	int				z;
+	int				last_z;
 	t_pos			**pos_array;
 
 	pos_array = (t_pos **)malloc((size + 1) * sizeof(t_pos *));
@@ -24,11 +52,13 @@ t_pos	**get_pos_array(char **coord, int y, int zoom)
 	{
 		i = 0;
 		x = 0;
+		last_z = 0;
 		while (coord[i] != NULL)
 		{
-			pos_array[i] = init_pos(x + HEIGHT / 4, y * zoom + WIDTH / 4, ft_atoi(coord[i]));
-			x += zoom;
+			z = ft_atoi(coord[i]);
+			pos_array[i] = get_curr_pos(&x, &y, &z, last_z, zoom, i);
 			++i;
+			last_z = z;
 		}
 		pos_array[i] = NULL;
 	}
