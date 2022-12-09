3194c5824648c6619a77f4af8a78b5ac88aedea0
romann-broque <kinoide56@gmail.com>
2022-12-09 17:12:54 +0100

    refacto print_map

--- srcs/print/print_map.c
+++ srcs/print/print_map.c
@@ -6,19 +6,28 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 15:44:38 by rbroque           #+#    #+#             */
-/*   Updated: 2022/12/09 16:29:53 by rbroque          ###   ########.fr       */
+/*   Updated: 2022/12/09 17:12:33 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+static void	link_pos(t_pos *pos1, t_pos *pos2, t_win *window)
+{
+	int	max_z;
+
+	if (pos2 != NULL)
+	{
+		max_z = get_max(pos1->z, pos2->z);
+		print_line(pos1, pos2, window->data, WHITE + max_z * BLUE);
+	}
+}
+
 void	print_map(t_pos ***pos_array, t_win *window)
 {
 	size_t	x;
 	size_t	y;
-	int		max_z;
-	t_pos	*pos1;
-	t_pos	*pos2;
+	t_pos	*curr_pos;
 
 	y = 0;
 	while (pos_array[y] != NULL)
@@ -26,20 +35,10 @@ void	print_map(t_pos ***pos_array, t_win *window)
 		x = 0;
 		while (pos_array[y][x] != NULL)
 		{
-			pos1 = pos_array[y][x];
-			if (pos_array[y][x + 1] != NULL)
-			{
-				pos2 = pos_array[y][x + 1];
-				max_z = get_max(pos1->z, pos2->z);
-				print_line(pos1, pos2, window->data, WHITE + max_z * BLUE);
-			}
-			if (pos_array[y + 1] != NULL
-					&& pos_array[y + 1][x] != NULL)
-			{
-				pos2 = pos_array[y + 1][x];
-				max_z = get_max(pos1->z, pos2->z);
-				print_line(pos1, pos2, window->data, WHITE + max_z * BLUE);
-			}
+			curr_pos = pos_array[y][x];
+			link_pos(curr_pos, pos_array[y][x + 1], window);
+			if (pos_array[y + 1] != NULL)
+				link_pos(curr_pos, pos_array[y + 1][x], window);
 			++x;
 		}
 		++y;
