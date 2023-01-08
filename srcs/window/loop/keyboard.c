13c81fb7bbdc5a4c420702d050ae926353050dc6
romann-broque <kinoide56@gmail.com>
2023-01-08 14:29:23 +0100

    clean key_press mapping

--- srcs/window/loop/keyboard.c
+++ srcs/window/loop/keyboard.c
@@ -6,20 +6,20 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 11:52:06 by rbroque           #+#    #+#             */
-/*   Updated: 2023/01/08 12:23:31 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/01/08 14:28:46 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	key_press(int key, t_win *window)
+static int	browse_mapping(
+								t_event_mapping *mapping,
+								const int key,
+								t_win *window
+							)
 {
-	static t_event_mapping	mapping[] = {
-	{.key = ESCAPE_KEY, .event = close_window},
-	{.key = NO_KEY, .event = NULL},
-	};
-	int						ret_val;
-	size_t					i;
+	int		ret_val;
+	size_t	i;
 
 	ret_val = EXIT_SUCCESS;
 	i = 0;
@@ -30,4 +30,14 @@ int	key_press(int key, t_win *window)
 		++i;
 	}
 	return (ret_val);
+}
+
+int	key_press(const int key, t_win *window)
+{
+	static t_event_mapping	mapping[] = {
+	{.key = ESCAPE_KEY, .event = close_window},
+	{.key = NO_KEY, .event = NULL},
+	};
+
+	return (browse_mapping(mapping, key, window));
 }
