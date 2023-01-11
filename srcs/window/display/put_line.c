2e76a7a860c865889e0a57d7eee94b5b051851ba
romann-broque <kinoide56@gmail.com>
2023-01-11 01:09:45 +0100

    put_line in progress

--- srcs/window/display/put_line.c
+++ srcs/window/display/put_line.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 14:10:16 by rbroque           #+#    #+#             */
-/*   Updated: 2023/01/10 14:24:00 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/01/10 23:48:08 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,17 @@ void	put_line(t_data *data, int x1, int y1, const int x2, const int y2)
 			e += dx;
 		}
 	}
+}
+
+void	display_line_test(t_win *window)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < HEIGHT / 2)
+	{
+		put_line(&window->data, 0, i, WIDTH / 2, HEIGHT / 2);
+		++i;
+	}
+	mlx_put_image_to_window(window->mlx_ptr, window->win_ptr, window->data.img, 0, 0);
 }
