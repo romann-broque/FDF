8b127b0d9cc6161c58ba78bf2cd546b6e48d5be4
romann-broque <kinoide56@gmail.com>
2023-01-12 13:19:26 +0100

    add angle

--- srcs/window/loop/keyboard.c
+++ srcs/window/loop/keyboard.c
@@ -6,11 +6,29 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 11:52:06 by rbroque           #+#    #+#             */
-/*   Updated: 2023/01/11 18:55:46 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/01/12 13:10:07 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+	// cpy_matrix(window->refr_matrix, window->orig_matrix);
+	// set_offset(window->refr_matrix, window->transform);
+	// window->transform->center = get_center(window->transform);
+	// mlx_destroy_image(window->mlx_ptr, window->data->img);
+	// window->data = init_data(window->mlx_ptr, window->data->contrast);
+	// free(tmp); // assign instead of realloc
+	// print_map(window->refr_matrix, window, WHITE);
+	// mlx_put_image_to_window(window->mlx_ptr, window->win_ptr, window->data->img, 0, 0);
+
+static void	refresh(t_win *window)
+{
+	mlx_destroy_image(window->mlx_ptr, window->data.img);
+	init_data(window->mlx_ptr, &window->data);
+	transform_map(&window->map);
+	display_window(window);
+	mlx_put_image_to_window(window->mlx_ptr, window->win_ptr, window->data.img, 0, 0);
+}
 
 static int	browse_mapping(
 								t_event_mapping *mapping,
@@ -29,6 +47,7 @@ static int	browse_mapping(
 			ret_val = mapping[i].event(window);
 		++i;
 	}
+	refresh(window);
 	return (ret_val);
 }
 
