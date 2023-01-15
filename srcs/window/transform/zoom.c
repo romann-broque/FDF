d8147a221d70eef9a2c8d72f47635863d807de56
romann-broque <kinoide56@gmail.com>
2023-01-15 01:49:06 +0100

    just change the zoom

--- srcs/window/transform/zoom.c
+++ srcs/window/transform/zoom.c
@@ -6,16 +6,16 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 16:02:46 by rbroque           #+#    #+#             */
-/*   Updated: 2023/01/13 16:11:44 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/01/15 01:48:12 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	zoom_vertex(t_vertex *vertex, const size_t y_index, const size_t x_index, const size_t zoom)
+static void	zoom_vertex(t_vertex *vertex, const size_t zoom)
 {
-	vertex->x += x_index * zoom;
-	vertex->y += y_index * zoom;
+	vertex->x *= zoom;
+	vertex->y *= zoom;
 }
 
 void	zoom(const t_map *map)
@@ -29,7 +29,7 @@ void	zoom(const t_map *map)
 		j = 0;
 		while (j < map->x_size)
 		{
-			zoom_vertex(&(map->vcpy[i][j]), i, j, map->zoom);
+			zoom_vertex(&(map->vcpy[i][j]), map->zoom);
 			++j;
 		}
 		++i;
