f0a0003ca9ef0cd6809ba7c9df1eb5f16c5596d2
romann-broque <kinoide56@gmail.com>
2023-01-15 17:36:47 +0100

    fix gnl issue

--- libft/srcs/gnl/get_next_line.c
+++ libft/srcs/gnl/get_next_line.c
@@ -6,17 +6,26 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 15:12:42 by rbroque           #+#    #+#             */
-/*   Updated: 2022/11/11 21:44:57 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/01/15 17:34:49 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	get_absolute_index(char *buffer, const char c)
+{
+	const ssize_t	index = index_of(buffer, c);
+
+	if (index == -1)
+		return (ft_strlen(buffer));
+	return (index);
+}
+
 static t_line_status	get_line_from_buff(char **line, char *buffer)
 {
 	size_t	index;
 
-	index = index_of(buffer, '\n');
+	index = get_absolute_index(buffer, '\n');
 	if (*buffer != '\0')
 	{
 		*line = ft_strnjoin(*line, buffer, index + 1);
@@ -50,7 +59,7 @@ static t_line_status	fill_line_from_file(char **line,
 
 static void	get_line(int fd, char **line)
 {
-	static char		rest[OPEN_MAX][BUFFER_SIZE + 1] = {0};
+	static char		rest[OPEN_MAX][BUFFER_SIZE + 1] = {{0}};
 
 	if (get_line_from_buff(line, rest[fd]) == INVALID_LINE)
 		fill_line_from_file(line, rest[fd], fd);
